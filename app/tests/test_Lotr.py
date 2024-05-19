ff57581f2e81a2eccffe3f8adc4a30c4c4873e66
andreichichi <andrei.chiran2001@gmail.com>
2024-05-19 20:02:29 +0300

    testcase logic v2

--- app/tests/test_Lotr.py
+++ app/tests/test_Lotr.py
@@ -12,9 +12,11 @@ def test_descriere():
     descriere = description.returneaza_descriere()
     if descriere:
         logger.info(f"Se genereaza urmatoarea descriere {descriere}")
+        print(f"Se genereaza urmatoarea descriere {descriere}")
         assert True
     else:
         logger.error(f"Nu s-a detectat nicio descriere.")
+        print(f"Nu s-a detectat nicio descriere.")
         assert False
     
 
@@ -24,7 +26,9 @@ def test_rating():
 
     if rate == "5/5":
         logger.info(f"Ratingul este cel asteptat:{rate}\n")
+        print(f"Ratingul este cel asteptat:{rate}\n")
         assert True
     else:
         logger.error(f"Ratingul nu este cel asteptat:{rate}\n")
+        print(f"Ratingul nu este cel asteptat:{rate}\n")
         assert False 
