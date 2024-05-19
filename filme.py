61c436e62a7a88b6dc48628ad7e6cb25689c2759
andreichichi <andrei.chiran2001@gmail.com>
2024-05-19 18:48:16 +0300

    Full commit

--- filme.py
+++ filme.py
@@ -1,32 +1,38 @@
 import sys
 
 from flask import Flask, url_for
+from flask import render_template
+import app.lib.description as dscr
+import app.lib.rating as rate
+import app.tests.test_Lotr as tst
+
 
 #from app.lib import network
 #from app.lib import linux
 #from app.grafice.exemplu_func_grad_2 import valori_x, valori_y, genereaza_grafice
 
-print('filme')
-
+print(tst.test_rating())
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    ret = "<h1>FILME</h1>"
     
-   
-    ret += "<pre>"
-   
-    ret += " - -test1\n"
-    ret += " - -test2"
-    
-    ret += "</pre>"
-    
-    ret += " - -test3\n"
-    ret += " - -test4"
-    
-    return ret
-    
+    return render_template("index.html")
+
+@app.route("/index_Lotr")
+def home():
+    description = dscr.returneaza_descriere()
+    rating = rate.returneaza_rating()
+    return render_template("index_Lotr.html", description = description, rating = rating)
+
+
+@app.route("/cast_Lotr")
+def cast():
+    return render_template("cast_Lotr.html")
+
+@app.route("/description_Lotr")
+def description():
+    return render_template("description_Lotr.html")
 
     
 @app.cli.command()
@@ -39,3 +45,7 @@ def test():
     """
     import pytest
     sys.exit(pytest.main(["."]))
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
