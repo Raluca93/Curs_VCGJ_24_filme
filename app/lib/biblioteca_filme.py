6cc7a0d26928978bf4f77f70c656c7702a26fcee
PCristian2001 <predacristian2001@gmail.com>
2024-05-25 00:34:11 +0300

    git ignore changed

---
+++ app/lib/biblioteca_filme.py
@@ -0,0 +1,125 @@
+def descriere_film():
+    descriere = """
+    <html>
+    <head>
+        <title>Descriere</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #052108;
+            }
+            header {
+                background-color: #0a6b14;
+                color: #fff;
+                padding: 10px 0;
+                text-align: center;
+            }
+            h1 {
+                margin: 0;
+            }
+            nav {
+                margin: 20px;
+                text-align: center;
+            }
+            nav a {
+                margin: 0 10px;
+                text-decoration: none;
+                color: #333;
+            }
+            .container {
+                padding: 20px;
+                background-color: #64e872;
+                margin: 20px auto;
+                width: 80%;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                text-align: center;
+            }
+            img {
+                max-width: 100%;
+                height: auto;
+                margin: 20px 0;
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>Descriere</h1>
+        </header>
+        <nav>
+            <a href="/">Acasă</a>
+            <a href="/descriere/central_intelligence">Descriere</a>
+            <a href="/recenzii/central_intelligence">Recenzii</a>
+        </nav>
+        <div class="container">
+            <img src="https://m.media-amazon.com/images/M/MV5BMTY3NTAxYTEtYWJkZS00NGEyLWIzZmItZmM4NzJlYmEwMjMyXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_FMjpg_UX1000_.jpg">
+            <p>When the Ed Boys' latest plan goes awry and leads to the ruin of the Cul-De-Sac, the children turn against them. Now, the Eds have no choice but to seek out the one person who may be able to save them: Eddy's Brother. But they also have to contend with The Kankers chasing them as well.</p>
+                    </div>
+    </body>
+    </html>
+    """
+    return descriere
+
+def recenzii_film():
+    recenzii = """
+    <html>
+    <head>
+        <title>Recenzii</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #052108;
+            }
+            header {
+                background-color: #0a6b14;
+                color: #fff;
+                padding: 10px 0;
+                text-align: center;
+            }
+            h1 {
+                margin: 0;
+            }
+            nav {
+                margin: 20px;
+                text-align: center;
+            }
+            nav a {
+                margin: 0 10px;
+                text-decoration: none;
+                color: #333;
+            }
+            .container {
+                padding: 20px;
+                background-color: #64e872;
+                margin: 20px auto;
+                width: 80%;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>Recenzii</h1>
+        </header>
+        <nav>
+            <a href="/">Acasă</a>
+            <a href="/descriere/central_intelligence">Descriere</a>
+            <a href="/recenzii/central_intelligence">Recenzii</a>
+        </nav>
+        <div class="container">
+            <ol>
+                <p><b>Prehistory96: The Great Ed, Edd n Eddy's Big Picture Show is literally Immense. The Greatest of All movies, and the last of their kind. No other movie will replace it not even shows of any channel will be able to compete with The Official Movie EVENT. The Ed, Edd n Eddy Big Picture Show is way more complex than any movie played on Cartoon Network, Nickelodeon, Disney Channel, or any other channel to the viewers. (Ed, Edd n Eddy's Big Picture Show)Respectively deserves more than a review. Last of The Cartoon Cartoons, Last of The Fridays Shows and know the last of their kind. Cow & Chicken, I Am Weasel, Scooby Doo, Johnny Bravo, Dexter's Laboratory, Tom & Jerry, The PowerPuff Girls, The Grim Adventures of Billy & Mandy, Codename:Kids Next Door, Sameri Jack, Foster's Home for Imagenary Friends, Teen Titans, but only Ed, Edd n Eddy continued until the movie of which told it all. Absolutely no upcoming shows like Adventure Time & Generator Rex or The Regular Show. Ed, Edd n Eddy shall be missed and remember-ED.</p>
+                <p><b>I was a huge "Ed, Edd n Eddy" fan when I was younger. Now I'm much older and beyond these types of cartoon. But I still had to check out this movie for the nostalgia. I loved the show when I was younger, and enjoyed the movie quite a bit.
+
+All the typical zaniness that you'd expect from the Eds and had seen in the television series, plus more, is present in the movie and very reasonably so.
+The only thing I can really complain about was the ending, which I won't reveal. I think it seemed a bit rushed. Even though it's a cartoon, I see it as unrealistic, even in the AKA-Cartooniverse. The majority of people I have talked to about the movie saw no problem with it, though, so maybe I'm just too bitter.
+Overall It was a nice little 90 minute trip back to my childhood. If you loved the series, you need to see this movie.</p>
+           </ol>
+        </div>
+    </body>
+    </html>
+    """
+    return recenzii
