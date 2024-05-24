c9a5edb37f4bc2f56b0a89d4d364d10c121adef1
PCristian2001 <predacristian2001@gmail.com>
2024-05-25 00:25:16 +0300

    first push

---
+++ app/preda_andrei_cristian_441d_filme.py
@@ -0,0 +1,96 @@
+import sys
+import flask
+
+from lib import biblioteca_filme
+
+print('filme')
+
+__filme__ = "filme_app"
+app = flask.Flask(__filme__)
+
+# Parametrii globali pentru descriere si recenzii
+
+@app.route("/", methods=['GET'])
+def index():
+    ret = """
+    <html>
+    <head>
+        <title>Filme</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #052108;
+            }
+            header {
+                background-color: #0a6b14;
+                color: #fff;
+                padding: 10px 0;
+                text-align: center;
+            }
+            h1 {
+                margin: 0;
+            }
+            nav {
+                margin: 20px;
+                text-align: center;
+            }
+            nav a {
+                margin: 0 10px;
+                text-decoration: none;
+                color: #333;
+            }
+            .container {
+                padding: 20px;
+                background-color: #fff;
+                margin: 20px auto;
+                width: 80%;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <h1>Film</h1>
+        </header>
+        <nav>
+            <a href="/">Acasă</a>
+            <a href="/descriere/Ed_Edd_n_Eddys_Big_Picture_Show">Descriere</a>
+            <a href="/recenzii/Ed_Edd_n_Eddys_Big_Picture_Show">Recenzii</a>
+        </nav>
+        <div class="container">
+        Pagina Principala a Aplicatiei Web.
+        </div>
+    </body>
+    </html>
+    """
+    return ret
+
+@app.route("/descriere/Ed_Edd_n_Eddys_Big_Picture_Show", methods=['GET'])
+def get_descriere():
+    descriere=biblioteca_filme.descriere_film()
+    return descriere
+
+@app.route("/recenzii/Ed_Edd_n_Eddys_Big_Picture_Show", methods=['GET'])
+def get_recenzii():
+    recenzii=biblioteca_filme.recenzii_film()
+    return recenzii
+
+@app.cli.command()
+def test():
+    """
+    Rulare 'unit tests'
+
+    Apelare pytest din aplicatia systest, cu ajutorul comenzii flask.
+    """
+    import pytest
+    errno = pytest.main(["-x", "app/test"])
+    sys.exit(errno)
+    #sys.exit(pytest.main(["."]))
+
+if __name__ == "__main__":
+
+#if __filme__ == "filme_app":
+    app.run()
+#app.run(host = "127.0.0.1", port = 5001)
