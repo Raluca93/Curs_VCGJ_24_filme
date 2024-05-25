5fde31db5aae6acfe26ed7932cd3f615162bd970
alexandra1603 <alexandra.marcu1603@stud.etti.upb.ro>
2024-05-25 17:34:27 +0300

    Third commit-Jenkinsfile

---
+++ app/lib/biblioteca_filme.py
@@ -0,0 +1,21 @@
+def distributie_barbie():
+    return f'''<h3>
+    Margot Robbie în rolul lui Barbie - protagonista blondă care începe să-și pună întrebări despre existența sa perfectă.<br>
+    Ryan Gosling în rolul lui Ken - partenerul lui Barbie, care, de asemenea, se luptă cu propria criză de identitate.<br>
+    America Ferrera în rolul lui Gloria - o prietenă din lumea reală care îi ajută pe Barbie și Ken să navigheze prin complexitățile vieții exterioare.<br>
+    Kate McKinnon în rolul lui Janet - un personaj misterios cu intenții neclare.<br>
+    Simu Liu în rolul lui Allan - un alt Ken din Barbieland, care joacă un rol important în poveste.<br>
+    Issa Rae în rolul Președintei Barbies - liderul comunității Barbie, care deține multe răspunsuri la întrebările lui Barbie.<br>
+    Michael Cera în rolul lui Business Ken - un Ken orientat spre afaceri din Barbieland.<br>
+    Emma Mackey în rolul lui Skipper - sora rebelă a lui Barbie, care îi provoacă pe ceilalți să gândească diferit.<br>
+    Will Ferrell în rolul CEO-ului Mattel - antagonistul filmului, care dorește să mențină status quo-ul în Barbieland.
+    </h3>'''
+
+def actiune_barbie():
+    return f'''<h3>
+    Filmul "Barbie" începe în Barbieland, un univers în care totul pare perfect. Barbie (Margot Robbie) trăiește o viață ideală, însă începe să observe anumite anomalii și repetitivitate în comportamentul celor din jurul său. Aceasta o determină să înceapă să se întrebe dacă există mai mult dincolo de lumea ei colorată și fără griji.<br><br>
+    Îndrumată de Președinta Barbies (Issa Rae), Barbie decide să pornească într-o aventură în "lumea reală" împreună cu Ken (Ryan Gosling). Ajunși în lumea reală, cei doi se confruntă cu provocări și situații care le pun la încercare concepțiile despre perfecțiune și fericire. Ei întâlnesc personaje diverse, inclusiv pe Gloria (America Ferrera) și pe Janet (Kate McKinnon), care le oferă perspective diferite asupra vieții și asupra importanței autenticității.<br><br>
+    Pe măsură ce se adaptează la această nouă lume, Barbie și Ken învață despre responsabilitate, diversitate și imperfecțiuni. Barbie descoperă că autenticitatea este mai valoroasă decât perfecțiunea superficială la care era obișnuită. Ea și Ken se întorc în Barbieland transformați, hotărâți să împărtășească lecțiile învățate și să inspire schimbări pozitive în comunitatea lor.<br><br>
+    Filmul se încheie cu o confruntare între Barbie, Ken și CEO-ul Mattel (Will Ferrell), care încearcă să păstreze controlul asupra Barbielandului. Cu ajutorul noilor lor înțelegeri și aliați, Barbie și Ken reușesc să aducă un nou început pentru Barbieland, marcând începutul unei ere de acceptare și diversitate.
+    </h3>'''
+
