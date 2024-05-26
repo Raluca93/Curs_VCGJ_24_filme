aa10be70365160d9d07a652d455d6e8e33e0de68
Gabriel <allex20156@yahoo.com>
2024-05-27 02:57:45 +0300

    Added docker container creation stage to pipeline On branch dev-Gabriel

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -23,7 +23,6 @@ def test_title(client):
     # Send a GET request to the specified static file
     # Check that the status code is 200
     ans_data = biblioteca.titlu_film(client)
-    # Check that the response contains some expected content from the CSS file
     expected_content = b"<h1>Godzilla</h1>"
     if(expected_content in ans_data):
         logger.info("Pagine WEB conține titlul așteptat")
@@ -33,11 +32,10 @@ def test_title(client):
         assert False
 
 def test_description(client):
-    logger.info("Trimitere cerere GET către /godzilla")
+    logger.info("Trimitere cerere GET către /description")
     # Send a GET request to the specified static file
     # Check that the status code is 200
     ans_data = biblioteca.titlu_descriere(client)
-    # Check that the response contains some expected content from the CSS file
     expected_content = b"<h1>Godzilla - Description</h1>"
     if(expected_content in ans_data):
         logger.info("Pagine WEB conține descrierea asteptata")
