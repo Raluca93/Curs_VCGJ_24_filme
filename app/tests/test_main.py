d2512ff202271e9d436483b5342a251893d444a0
Gabriel <allex20156@yahoo.com>
2024-05-27 02:34:07 +0300

    MOdify test_main

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,12 +3,11 @@ import logging
 import sys
 import os
 
-from main import app
 from lib import biblioteca_filme as biblioteca
+from main import app
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-
 @pytest.fixture
 def client():
     # Creere de client pentru aplicatia Flask pentru testare
@@ -17,9 +16,7 @@ def client():
         "TESTING": True,
     })
         yield client
-        
 logger = logging.getLogger(__name__)
-
 def test_title(client):
     
     logger.info("Trimitere cerere GET către /godzilla")
@@ -28,7 +25,7 @@ def test_title(client):
     ans_data = biblioteca.titlu_film(client)
     # Check that the response contains some expected content from the CSS file
     expected_content = b"<h1>Godzilla</h1>"
-    if (expected_content in ans_data):
+    if(expected_content in ans_data):
         logger.info("Pagine WEB conține titlul așteptat")
         assert True
     else:
