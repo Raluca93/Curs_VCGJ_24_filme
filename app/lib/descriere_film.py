be7c8e00fdcf6a75c17eaeafe905bf4a1e44da03
alexslavoiu <slavoiugabriel@yahoo.com>
2024-05-26 23:54:35 +0300

    mesaj1

---
+++ app/lib/descriere_film.py
@@ -0,0 +1,6 @@
+import subprocess
+
+
+def returneaza_descriere():
+	descriere = "A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her ten year old son John from an even more advanced and powerful cyborg."
+	return descriere
