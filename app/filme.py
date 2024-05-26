0e802c153a2e61022e70e2404416f59f7281aadd
Ruj0iu <rujoiualex@gmail.com>
2024-05-27 02:40:27 +0300

    Schimbare locatie filme.py si activeaza_

---
+++ app/filme.py
@@ -0,0 +1,76 @@
+from flask import Flask , url_for
+from app.lib import lib_filme as filme
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    ret = "<h1>Filme</h1>"
+    
+    ret += f"<a href={url_for('V_For_Vendetta')}>V for Vendetta - Rujoiu Alexandru-Valentin</a> <br/>"
+
+    return ret
+
+
+    
+@app.route('/V_For_Vendetta', methods=['GET'])
+def V_For_Vendetta():
+	quotes = filme.quotes()
+	descriere = filme.descriere_film() 
+	
+	ret = "<h1>V for Vendetta</h1>"
+	
+	#Linkuri
+	ret += f"<a href={url_for('index')}>[Filme]</a> | "
+	ret += f"<a href={url_for('view_quotes_V')}>[quotes]</a> | "
+	ret += f"<a href={url_for('view_descriere_V')}>[descriere]</a>"
+	
+	ret += "<h2>Descriere: </h2>"
+	
+	ret += descriere 
+	
+	ret += "<h2>quotes: </h2>"
+	ret += quotes
+	
+	
+	
+	return ret
+
+
+@app.route('/V_For_Vendetta/quotes', methods=['GET'])
+def view_quotes_V():
+    quotes =filme.quotes()  
+    
+    ret = "<h1>Quotes from V:</h1>"
+    ret += f"<a href={url_for('index')}>[Filme]</a> | "
+    ret += f"<a href={url_for('V_For_Vendetta')}>[V for Vendetta]</a> <br/> <br/>"
+    ret += quotes
+    
+    return ret
+
+@app.route('/V_For_Vendetta/descriere', methods=['GET'])
+def view_descriere_V():
+    descriere =filme.descriere_film()   
+    
+    ret = "<h1>Descriere V for Vendetta:</h1>"
+    ret += f"<a href={url_for('index')}>[Filme]</a> | "
+    ret += f"<a href={url_for('V_For_Vendetta')}>[V for Vendetta]</a> <br/> <br/>"
+    ret += descriere
+    
+    return ret
+    
+    
+@app.cli.command()
+def test():
+    """
+    Rulare 'unit tests'
+    
+    Apelare pytest din aplicatia systest, cu ajutorul comenzii flask.
+    
+    """
+    import pytest
+    sys.exit(pytest.main(["-x","app/test"]))
+    
+if __name__ == '__main__':
+    app.run(host='127.0.0.1')
