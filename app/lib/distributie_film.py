dfbd80714094925460491a28ccf876a6417b22bb
alexslavoiu <slavoiugabriel@yahoo.com>
2024-05-26 23:57:19 +0300

    mesaj2

---
+++ app/lib/distributie_film.py
@@ -0,0 +1,5 @@
+import subprocess
+
+def returneaza_distributie():
+	distributie = "In the Year 1992, at the Academy Award, Terminator 2: Judgment Day received nominations for Best Cinematography and Best Film Editing , and won for Best Sound, Best Effects, Sound Effects Editing, Visual Effects and Best Makeup."
+	return distributie
