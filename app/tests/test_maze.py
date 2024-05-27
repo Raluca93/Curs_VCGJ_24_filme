d4bc4e75554d15c01fdfee333a43d9615c9cad10
octavian <octavian.theodor162001@gmail.com>
2024-05-27 10:33:12 +0300

    Readme.md fixed photos + lib

--- app/tests/test_maze.py
+++ app/tests/test_maze.py
@@ -6,7 +6,7 @@ import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 from filme import app
-from lib import biblioteca_filme as biblioteca
+from app.lib import biblioteca_filme_maze as biblioteca
 
 @pytest.fixture
 def client():
