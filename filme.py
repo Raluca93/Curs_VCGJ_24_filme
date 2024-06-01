d1fb75df315848b3bbb2f0827ef47532fa0e5938
Ruj0iu <rujoiualex@gmail.com>
2024-06-01 23:41:00 +0300

    Rezolved bugs , and recreated all

--- filme.py
+++ filme.py
@@ -8,54 +8,54 @@ app = Flask(__name__)
 def index():
     ret = "<h1>Filme</h1>"
     
-    ret += f"<a href={url_for('V_For_Vendetta')}>V for Vendetta - Rujoiu Alexandru-Valentin</a> <br/>"
+    ret += f"<a href={url_for('One_Piece_RED')}>One Piece RED - Toader Florin</a> <br/>"
 
     return ret
 
 
     
-@app.route('/V_For_Vendetta', methods=['GET'])
-def V_For_Vendetta():
-	quotes = filme.quotes()
+@app.route('/One_Piece_RED', methods=['GET'])
+def One_Piece_RED():
+	comentari = filme.comentari()
 	descriere = filme.descriere_film() 
 	
-	ret = "<h1>V for Vendetta</h1>"
+	ret = "<h1>One Piece RED</h1>"
 	
 	#Linkuri
 	ret += f"<a href={url_for('index')}>[Filme]</a> | "
-	ret += f"<a href={url_for('view_quotes_V')}>[quotes]</a> | "
-	ret += f"<a href={url_for('view_descriere_V')}>[descriere]</a>"
+	ret += f"<a href={url_for('view_comentari')}>[comentari]</a> | "
+	ret += f"<a href={url_for('view_descriere')}>[descriere]</a>"
 	
 	ret += "<h2>Descriere: </h2>"
 	
 	ret += descriere 
 	
-	ret += "<h2>quotes: </h2>"
-	ret += quotes
+	ret += "<h2>comentari: </h2>"
+	ret += comentari
 	
 	
 	
 	return ret
 
 
-@app.route('/V_For_Vendetta/quotes', methods=['GET'])
-def view_quotes_V():
-    quotes =filme.quotes()  
+@app.route('/One_Piece_RED/comentari', methods=['GET'])
+def view_comentari():
+    comentari =filme.comentari()  
     
-    ret = "<h1>Quotes from V:</h1>"
+    ret = "<h1>comentari:</h1>"
     ret += f"<a href={url_for('index')}>[Filme]</a> | "
-    ret += f"<a href={url_for('V_For_Vendetta')}>[V for Vendetta]</a> <br/> <br/>"
-    ret += quotes
+    ret += f"<a href={url_for('One_Piece_RED')}>[One Piece RED]</a> <br/> <br/>"
+    ret += comentari
     
     return ret
 
-@app.route('/V_For_Vendetta/descriere', methods=['GET'])
-def view_descriere_V():
+@app.route('/One_Piece_RED/descriere', methods=['GET'])
+def view_descriere():
     descriere =filme.descriere_film()   
     
-    ret = "<h1>Descriere V for Vendetta:</h1>"
+    ret = "<h1>Descriere One Piece RED:</h1>"
     ret += f"<a href={url_for('index')}>[Filme]</a> | "
-    ret += f"<a href={url_for('V_For_Vendetta')}>[V for Vendetta]</a> <br/> <br/>"
+    ret += f"<a href={url_for('One_Piece_RED')}>[One Piece RED]</a> <br/> <br/>"
     ret += descriere
     
     return ret
