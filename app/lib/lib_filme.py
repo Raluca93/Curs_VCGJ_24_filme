bec69401caf43938d66726685ca3853aa8d7be0d
Ruj0iu <101740245+Ruj0iu@users.noreply.github.com>
2024-06-02 00:13:42 +0300

    Add files via upload

---
+++ app/lib/lib_filme.py
@@ -0,0 +1,5 @@
+def descriere_film():
+    return "One Piece Film: Red is a 2022 Japanese animated musical fantasy action-adventure film directed by Gorō Taniguchi and produced by Toei Animation.[3][4] It is the fifteenth feature film of the One Piece film series, based on the manga of the same name written and illustrated by Eiichiro Oda."
+
+def comentari():
+    return "Watched it today, and j have to say it was a solid movie, idk why people keep on saying it's like a musical there is only 4 songs😂 and their bangers too, the hype is crazy too, the fight against the demon king was lit, especially shanks 😱, there was also a few frames where we could see gear 5 luffy, I was hyped tf uped when I saw gear 5 officially animated for the first time , the animation was fire , watched it in 4k 60fps everything was super smooth the fight scenes were lit, zero especially was sick af , the usual , sanji was also fire , i would rate this movie a solid 8.7/10solid movie, defo should check it out."
