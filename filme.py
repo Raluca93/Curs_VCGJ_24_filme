2fa50ea96a4377d641c63c89a1a12a640b385149
VasileValentin17 <vasilevalentin17@gmail.com>
2024-06-04 00:04:24 +0300

    Modificare filme.py, biblioteca_file.py + Adaugare fisier ruleaza_aplicatia

--- filme.py
+++ filme.py
@@ -2,38 +2,38 @@ import sys
 from flask import Flask, url_for
 from app.lib import biblioteca_filme
 
-actiune_iron_man = biblioteca_filme.actiune_iron_man()
-distributie_iron_man = biblioteca_filme.distributie_iron_man()
+descriere_matrix = biblioteca_filme.actiune_matrix()
+distributie_matrix = biblioteca_filme.distributie_matrix()
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def index():
-    ret = f"<h1><a href={url_for('iron_man')}>Ionescu Raluca 441D - Iron Man</a></h1>"
+    ret = f"<h1><a href={url_for('matrix')}>Vasile Valentin 441D - Iron Man</a></h1>"
     return ret
     
 
-@app.route("/iron_man", methods=['GET'])
-def iron_man():
-  ret = "<h1>Iron Man</h1><br>"
-  ret += f"<h2><a href={url_for('actiune')}>Actiune</a></h2>"  
+@app.route("/matrix", methods=['GET'])
+def matrix():
+  ret = "<h1>Matrix</h1><br>"
+  ret += f"<h2><a href={url_for('descriere')}>Descriere</a></h2>"  
   ret += f"<h2><a href={url_for('distributie')}>Distributie</a></h2>"
   return ret  
 	
-@app.route("/iron_man/distributie", methods=["GET"])
+@app.route("/matrix/distributie", methods=["GET"])
 def distributie():
   ret = "<h1>Distributie</h1>"
-  ret += f"<h2><a href={url_for('iron_man')}>Iron Man</a></h2> "
-  ret += f"<h2><a href={url_for('actiune')}>Actiune</a></h2>"
-  ret += distributie_iron_man
+  ret += f"<h2><a href={url_for('matrix')}>Matrix</a></h2> "
+  ret += f"<h2><a href={url_for('descriere')}>Descriere</a></h2>"
+  ret += distributie_matrix
   return ret
   
-@app.route("/iron_man/actiune", methods=["GET"])
-def actiune():
-  ret = "<h1>Actiune</h1>"
-  ret += f"<h2><a href={url_for('iron_man')}>Iron Man</a></h2>"
-  ret += f"<h2><a href={url_for('distributie')}>Distrinutie</a></h2>"
-  ret += actiune_iron_man
+@app.route("/matrix/descriere", methods=["GET"])
+def descriere():
+  ret = "<h1>Descriere</h1>"
+  ret += f"<h2><a href={url_for('matrix')}>Matrix</a></h2>"
+  ret += f"<h2><a href={url_for('distributie')}>Distributie</a></h2>"
+  ret += descriere_matrix
   return ret
   
   
