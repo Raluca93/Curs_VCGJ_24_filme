cb98e02222e693cfe644aa126f2b7a75e5051120
VasileValentin17 <vasilevalentin17@gmail.com>
2024-06-04 01:00:04 +0300

    Stergere images si README

--- app/tests/test_filme.py
+++ app/tests/test_filme.py
@@ -3,18 +3,18 @@ logger = logging.getLogger(__name__)
 
 import lib.biblioteca_filme as bf
 
-def test_actiune():
+def test_descriere():
 
-    actiune = bf.actiune_iron_man()
-    if actiune:
-        logger.info(f"Actiunea filmului este: {actiune}")
+    descriere = bf.actiune_matrix()
+    if descriere:
+        logger.info(f"Descrierea filmului este: {descriere}")
         assert True
     else:
         logger.error(f"Nu exista nicio descriere pentru acest film...")
         assert False
 
 def test_distributie():
-    distributie = bf.distributie_iron_man()
+    distributie = bf.distributie_matrix()
 
     if distributie:
         logger.info(f"Distributia filmului: \n{distributie}")
